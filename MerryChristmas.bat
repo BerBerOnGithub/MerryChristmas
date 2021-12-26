ef7cf56a83f5a88a9751f2de8fdec27cb03c3af3
BerBerOnGithub <86917016+BerBerOnGithub@users.noreply.github.com>
2021-12-26 18:56:49 +0100

    Add files via upload

---
+++ MerryChristmas.bat
@@ -0,0 +1,14 @@
+@echo off
+echo.                                                 Merry Christmas
+echo ________________________________________________________________________________________________________________________
+echo.
+echo.
+echo.
+echo.
+echo.
+:loop
+color ac
+TIMEOUT /T 1 /NOBREAK >NUL
+color ca
+TIMEOUT /T 1 /NOBREAK >NUL
+goto loop
